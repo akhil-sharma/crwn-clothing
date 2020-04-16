94d915605b86cb9834f57e0581c144cdaca837e5
Akhil Sharma <akhilsharma.cse@gmail.com>
2020-04-16 23:28:31 +0530

    implemented signin with google, signUp and, storing authenticated users in the firestore database.

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import SigIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss'
 
 const SignInAndSignUpPage = () => (
-    <SigIn/>
+    <div className="sign-in-and-sign-up">
+        <SigIn className='sign-in'/>
+        <SignUp className='sign-up'/>
+    </div>
 )
 
 export default SignInAndSignUpPage;
